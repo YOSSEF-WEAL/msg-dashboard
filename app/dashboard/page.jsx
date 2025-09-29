fc964a5630de9ed1097245483ab73b172987382a
Yossef weal <yweal423@gmaol.com>
2025-09-29 11:17:40 +0300

    test deploy

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -14,7 +14,7 @@ export default async function dashboardPage() {
   return (
     <div className="flex h-svh w-full items-center justify-center gap-2">
       <p>
-        Hello <span>{data.claims.email}</span>
+        Hello - <span>{data.claims.email}</span>
       </p>
       <LogoutButton />
     </div>
